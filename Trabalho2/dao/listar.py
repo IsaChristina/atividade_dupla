e05348905f126a85b316ad216e5f17c6559e7e41
unknown <900218@proway.treina>
2019-12-10 08:25:14 -0300

    OH

--- Trabalho2/dao/listar.py
+++ Trabalho2/dao/listar.py
@@ -6,30 +6,50 @@ class Listar(Conexao):
 
     def listar_pessoas(self):
 
+        lista = []
+
         self.cursor.execute('Select * from Pessoas')
+
+        for i in self.cursor.fetchall():
+            lista.append({'Id':i[0], 'Nome':i[1], 'Cpf':i[2], 'Telefone':i[3]})
         
-        return self.cursor.fetchall()
+        return lista
         
 # //////////////////
 
     def listar_funcionarios(self):
 
+        lista = []
+
         self.cursor.execute('Select * from Funcionarios')
+
+        for i in self.cursor.fetchall():
+            lista.append({'Id':i[0], 'Cargo':i[1], 'Salario':i[2], 'Id_pessoal':i[3]})
         
-        return self.cursor.fetchall()
+        return lista
         
 # //////////////////
 
     def listar_equipes(self):
 
+        lista = []
+
         self.cursor.execute('Select * from Equipes')
+
+        for i in self.cursor.fetchall():
+            lista.append({'Id':i[0], 'Pessoa_1':i[1], 'Pessoa_2':i[2], 'Pessoa_3':i[3], 'Pessoa_4':i[4]})
         
-        return self.cursor.fetchall()
+        return lista
         
 # //////////////////
 
     def listar_linguagens(self):
 
+        lista = []
+
         self.cursor.execute('Select * from Linguagens')
+
+        for i in self.cursor.fetchall():
+            lista.append({'Id':i[0], 'Nome':i[1]})
         
-        return self.cursor.fetchall()
+        return lista
